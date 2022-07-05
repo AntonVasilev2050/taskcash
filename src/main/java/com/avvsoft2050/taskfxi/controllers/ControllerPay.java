926bfcd66c47136f2a345bbbb22ec270ac7ebdc7
Anton2050 <avvsoft2050@gmail.com>
2022-07-05 14:58:02 +0300

    add buttonEnterReleased() method

--- src/main/java/com/avvsoft2050/taskfxi/controllers/ControllerPay.java
+++ src/main/java/com/avvsoft2050/taskfxi/controllers/ControllerPay.java
@@ -3,8 +3,12 @@ package com.avvsoft2050.taskfxi.controllers;
 import com.avvsoft2050.taskfxi.services.PayService;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
+
+import static javafx.scene.input.KeyCode.ENTER;
 
 @Component
 @FxmlView("/pay.fxml")
@@ -21,5 +25,12 @@ public class ControllerPay {
 
     public void buttonPayClicked(ActionEvent actionEvent) {
         payService.finishPayment(controllerCashMain.labelTotal, controllerCashMain.vBoxCart, textFieldPaymentAmount);
+    }
+
+    public void buttonEnterReleased(KeyEvent keyEvent) {
+        KeyCode keyCode = keyEvent.getCode();
+        if (keyCode == ENTER) {
+            payService.finishPayment(controllerCashMain.labelTotal, controllerCashMain.vBoxCart, textFieldPaymentAmount);
+        }
     }
 }
